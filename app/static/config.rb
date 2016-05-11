e9f3420a04b00767cdf6beb5e5bbcbe4f8b09d3e
Victor Aguilar C <victor@jvacx.com>
2016-05-11 12:21:09 -0400

    wagtail merged with static landing page

---
+++ app/static/config.rb
@@ -0,0 +1,8 @@
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "icons"
+javascripts_dir = "js"
+
+output_style = :expanded #:compressed #:expanded #or :nested or :compact or :compressed
